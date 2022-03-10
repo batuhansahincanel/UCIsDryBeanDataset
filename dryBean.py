e94866284a2648f634be90bedb7a309e3d301630
Batuhan <96589779+batuhansahincanel@users.noreply.github.com>
2022-03-10 19:01:33 +0300

    Update dryBean.py

--- dryBean.py
+++ dryBean.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-#Spreading it as train and test set
+#Spreading the dataset as train and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
@@ -25,7 +25,7 @@ X_test = sc.transform(X_test)
 
 #Building a Random Forest Model
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 50, criterion = 'entropy', random_state = 0, n_jobs = -1)
+classifier = RandomForestClassifier(n_estimators = 100, max_depth=100, criterion = 'entropy', random_state = 0, n_jobs = -1)
 classifier.fit(X_train, y_train)
 
 #Prediction 
@@ -42,6 +42,7 @@ cv_score = cross_val_score(classifier, X_train, y_train, cv = 10, n_jobs=-1)
 cv_score_mean = cv_score.mean()
 cv_score_std = cv_score.std()
 
-
+#Inversing back test set, and prediction to see exact classes
 X_test = sc.inverse_transform(X_test)
+y_test = le.inverse_transform(y_test)
 y_pred = le.inverse_transform(y_pred)
